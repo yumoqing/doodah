6f0f83b712d51c2688b52cdb502155d9394ba39e
yumoqing <yumoqing@gmail.com>
2021-02-23 08:55:39 +0800

    bugfix

--- loadplugins.py
+++ loadplugins.py
@@ -4,9 +4,14 @@ import sys
 from appPublic.folderUtils import listFile
 from appPublic.ExecFile import ExecFile
 
+from kivy.logger import Logger
+
 def load_plugins(p_dir):
 	ef = ExecFile()
 	pdir = os.path.join(p_dir, 'plugins')
+	if not os.path.isdir(pdir):
+		Logger.error('load_plugins:%s not exists', pdir)
+		return
 	sys.path.append(pdir)
 	for py in listFile(pdir, suffixs=['py'], rescursive=False):
 		ef.set('sys',sys)
